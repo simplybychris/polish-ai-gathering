88aa1271e7ae43a4acd3eb4868c3f169a5ea742d
Mateusz Kmieć <mateusz.kmiec@webmakers.expert>
2025-04-07 11:18:12 +0200

    chore(5): challenge categories

--- src/components/MeetupFirstChallenge.tsx
+++ src/components/MeetupFirstChallenge.tsx
@@ -35,6 +35,264 @@ const MeetupFirstChallenge = () => {
             <div className='absolute -bottom-3 -right-3 w-full h-full border-2 border-[#56FF8F] rounded-lg -z-10'></div>
           </div>
         </div>
+        <div>
+          <h2 className='text-3xl md:text-4xl font-bold text-center mb-16'>
+            <span className='text-white'>Kategorie konkursu</span>
+          </h2>
+          <div className='grid md:grid-cols-3 gap-8'>
+            <div className='tech-card flex flex-col items-center'>
+              <div className='h-12 w-12 rounded-full bg-[#56FF8F]/20 flex items-center justify-center mb-4'>
+                <svg
+                  width='83'
+                  height='83'
+                  viewBox='0 0 83 83'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <g filter='url(#filter0_i_255_870)'>
+                    <circle
+                      cx='41.5'
+                      cy='41.5'
+                      r='41.5'
+                      fill='#46FF86'
+                      fill-opacity='0.5'
+                    />
+                  </g>
+                  <path
+                    d='M16.083 41.5L41.4997 60.3643L66.9163 41.5'
+                    stroke='white'
+                    stroke-width='3'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  />
+                  <path
+                    d='M28.791 33.8751L41.4993 44.0418L54.2077 33.8751M38.9577 26.2501L41.4993 28.2835L44.041 26.2501L41.4993 24.2168L38.9577 26.2501Z'
+                    stroke='white'
+                    stroke-width='3'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  />
+                  <defs>
+                    <filter
+                      id='filter0_i_255_870'
+                      x='0'
+                      y='0'
+                      width='83'
+                      height='87'
+                      filterUnits='userSpaceOnUse'
+                      color-interpolation-filters='sRGB'
+                    >
+                      <feFlood flood-opacity='0' result='BackgroundImageFix' />
+                      <feBlend
+                        mode='normal'
+                        in='SourceGraphic'
+                        in2='BackgroundImageFix'
+                        result='shape'
+                      />
+                      <feColorMatrix
+                        in='SourceAlpha'
+                        type='matrix'
+                        values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+                        result='hardAlpha'
+                      />
+                      <feOffset dy='4' />
+                      <feGaussianBlur stdDeviation='5.15' />
+                      <feComposite
+                        in2='hardAlpha'
+                        operator='arithmetic'
+                        k2='-1'
+                        k3='1'
+                      />
+                      <feColorMatrix
+                        type='matrix'
+                        values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+                      />
+                      <feBlend
+                        mode='normal'
+                        in2='shape'
+                        result='effect1_innerShadow_255_870'
+                      />
+                    </filter>
+                  </defs>
+                </svg>
+              </div>
+              <h4 className='text-xl font-semibold mb-2 text-white'>
+                Idea/Pomysł
+              </h4>
+              <p className='text-gray-400  text-center'>
+                Startupy we wczesnej fazie koncepcyjnej
+              </p>
+            </div>
+            <div className='tech-card flex flex-col items-center'>
+              <div className='h-12 w-12 rounded-full bg-[#56FF8F]/20 flex items-center justify-center mb-4'>
+                <svg
+                  width='83'
+                  height='83'
+                  viewBox='0 0 83 83'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <g filter='url(#filter0_i_255_870)'>
+                    <circle
+                      cx='41.5'
+                      cy='41.5'
+                      r='41.5'
+                      fill='#46FF86'
+                      fill-opacity='0.5'
+                    />
+                  </g>
+                  <path
+                    d='M16.083 41.5L41.4997 60.3643L66.9163 41.5'
+                    stroke='white'
+                    stroke-width='3'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  />
+                  <path
+                    d='M28.791 33.8751L41.4993 44.0418L54.2077 33.8751M38.9577 26.2501L41.4993 28.2835L44.041 26.2501L41.4993 24.2168L38.9577 26.2501Z'
+                    stroke='white'
+                    stroke-width='3'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  />
+                  <defs>
+                    <filter
+                      id='filter0_i_255_870'
+                      x='0'
+                      y='0'
+                      width='83'
+                      height='87'
+                      filterUnits='userSpaceOnUse'
+                      color-interpolation-filters='sRGB'
+                    >
+                      <feFlood flood-opacity='0' result='BackgroundImageFix' />
+                      <feBlend
+                        mode='normal'
+                        in='SourceGraphic'
+                        in2='BackgroundImageFix'
+                        result='shape'
+                      />
+                      <feColorMatrix
+                        in='SourceAlpha'
+                        type='matrix'
+                        values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+                        result='hardAlpha'
+                      />
+                      <feOffset dy='4' />
+                      <feGaussianBlur stdDeviation='5.15' />
+                      <feComposite
+                        in2='hardAlpha'
+                        operator='arithmetic'
+                        k2='-1'
+                        k3='1'
+                      />
+                      <feColorMatrix
+                        type='matrix'
+                        values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+                      />
+                      <feBlend
+                        mode='normal'
+                        in2='shape'
+                        result='effect1_innerShadow_255_870'
+                      />
+                    </filter>
+                  </defs>
+                </svg>
+              </div>
+              <h4 className='text-xl font-semibold mb-2 text-white'>
+                MVP/Prototyp
+              </h4>
+              <p className='text-gray-400 text-center'>
+                Startupy posiadające MVP lub prototyp, które pozyskały
+                finansowanie nie większe niż 10 mln zł
+              </p>
+            </div>
+            <div className='tech-card flex flex-col items-center'>
+              <div className='h-12 w-12 rounded-full bg-[#56FF8F]/20 flex items-center justify-center mb-4'>
+                <svg
+                  width='83'
+                  height='83'
+                  viewBox='0 0 83 83'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <g filter='url(#filter0_i_255_870)'>
+                    <circle
+                      cx='41.5'
+                      cy='41.5'
+                      r='41.5'
+                      fill='#46FF86'
+                      fill-opacity='0.5'
+                    />
+                  </g>
+                  <path
+                    d='M16.083 41.5L41.4997 60.3643L66.9163 41.5'
+                    stroke='white'
+                    stroke-width='3'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  />
+                  <path
+                    d='M28.791 33.8751L41.4993 44.0418L54.2077 33.8751M38.9577 26.2501L41.4993 28.2835L44.041 26.2501L41.4993 24.2168L38.9577 26.2501Z'
+                    stroke='white'
+                    stroke-width='3'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  />
+                  <defs>
+                    <filter
+                      id='filter0_i_255_870'
+                      x='0'
+                      y='0'
+                      width='83'
+                      height='87'
+                      filterUnits='userSpaceOnUse'
+                      color-interpolation-filters='sRGB'
+                    >
+                      <feFlood flood-opacity='0' result='BackgroundImageFix' />
+                      <feBlend
+                        mode='normal'
+                        in='SourceGraphic'
+                        in2='BackgroundImageFix'
+                        result='shape'
+                      />
+                      <feColorMatrix
+                        in='SourceAlpha'
+                        type='matrix'
+                        values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+                        result='hardAlpha'
+                      />
+                      <feOffset dy='4' />
+                      <feGaussianBlur stdDeviation='5.15' />
+                      <feComposite
+                        in2='hardAlpha'
+                        operator='arithmetic'
+                        k2='-1'
+                        k3='1'
+                      />
+                      <feColorMatrix
+                        type='matrix'
+                        values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0'
+                      />
+                      <feBlend
+                        mode='normal'
+                        in2='shape'
+                        result='effect1_innerShadow_255_870'
+                      />
+                    </filter>
+                  </defs>
+                </svg>
+              </div>
+              <h4 className='text-xl font-semibold mb-2 text-white'>
+                Program AI first
+              </h4>
+              <p className='text-gray-400 text-center'>
+                Akceleratory, inkubatory oraz jednostki prowadzące programy
+                startupowe specjalizujące się w AI
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
