dd77a4c7f3a9a84004ae85aecf546f8d6e9d4dab
Mateusz Kmieć <mateusz.kmiec@webmakers.expert>
2025-04-08 09:59:50 +0200

    chore(5): replace video

--- src/components/MeetupVideo.tsx
+++ src/components/MeetupVideo.tsx
@@ -4,7 +4,7 @@ const MeetupVideo = () => {
       <iframe
         width='560'
         height='315'
-        src='https://www.youtube.com/embed/u31qwQUeGuM?si=wohMXbC_WD_Kc-YD'
+        src='https://www.youtube.com/embed/1jyqjWCsZTs?si=hZvfCNVDz4j2aJAK'
         title='YouTube video player'
         frameBorder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
